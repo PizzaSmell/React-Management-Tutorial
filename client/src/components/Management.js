0af4144b03853f7c9fbb197d6da170c474491d92
JKM <gyungm@gmail.com>
2024-06-10 22:42:33 +0900

    feat.로그인기능추가, 하위메뉴적용

--- client/src/components/Management.js
+++ client/src/components/Management.js
@@ -4,37 +4,43 @@ import LoadMenu from './SideMenu';
 import './Management.css';
 
 function Management() {
-    const location = useLocation();
-    const { name, team } = location.state || { name: '사용자', team: '팀' };
+  const location = useLocation();
+  const { name, team } = location.state || { name: '사용자', team: '팀' };
 
-    const path = location.pathname;
-    let managementType;
-    switch (path) {
-        case '/purchase-management':
-            managementType = '구매관리';
-            break;
-        case '/material-management':
-            managementType = '자재관리';
-            break;
-        case '/quality-management':
-            managementType = '품질관리';
-            break;
-        case '/production-management':
-            managementType = '생산관리';
-            break;
-        default:
-            managementType = '관리';
-    }
+  const path = location.pathname;
+  let managementType;
+  switch (path) {
+    case '/purchase-management':
+      managementType = '구매관리 - 거래처';
+      break;
+    case '/material-management':
+      managementType = '자재관리';
+      break;
+    case '/quality-management':
+      managementType = '품질관리';
+      break;
+    case '/production-management':
+      managementType = '생산관리';
+      break;
+    default:
+      managementType = '관리';
+  }
 
-    return (
-        <div className="management">
-            <LoadMenu />
-            <div className="content">
-                <h1>{managementType}</h1>
-                <p>여기에 {managementType} 관련 내용을 표시합니다.</p>
-            </div>
+  return (
+    <div className="management">
+      <LoadMenu />
+      <div className="content">
+        <div className="user-info">
+          {name}({team})님 접속!
         </div>
-    );
+        <div className="logout">
+          <a href="/">로그아웃</a>
+        </div>
+        <h1>{managementType}</h1>
+        <p>여기에 {managementType} 관련 내용을 표시합니다.</p>
+      </div>
+    </div>
+  );
 }
 
 export default Management;
--- client/src/components/SideMenu.js
+++ client/src/components/SideMenu.js
@@ -15,7 +15,7 @@ const MenuTitle = ({ clickEvt }) => (
     <span>
       <i className="fas fa-bars"></i>
     </span>{' '}
-    Menu{' '}
+    RODEMFOOD{' '}
     <span onClick={clickEvt} className="menu-toggle">
       <i className="fas fa-times not-rotated"></i>
     </span>
@@ -28,50 +28,81 @@ const SearchBar = () => (
   </div>
 );
 
-const MenuList = () => (
+const MenuList = ({ toggleSubMenu, openMenu }) => (
   <div className="menu-list custom-margin">
-    <Link to="/purchase-management">
-      <span>
-        <i className="fas fa-pen-fancy"></i>
-      </span>{' '}
-      구매관리
-    </Link>
-    <Link to="/material-management">
-      <span>
-        <i className="fas fa-project-diagram"></i>
-      </span>{' '}
-      자재관리
-    </Link>
-    <Link to="/quality-management">
-      <span>
-        <i className="fas fa-folder-open"></i>
-      </span>{' '}
-      품질관리
-    </Link>
-    <Link to="/production-management">
-      <span>
-        <i className="fas fa-chart-pie"></i>
-      </span>{' '}
-      생산관리{' '}
-      <span style={{ marginLeft: 'auto', width: 'auto' }}>
-        <i className="fas fa-chevron-down"></i>
-      </span>
-    </Link>
+    <div className="menu-item">
+      <Link to="#" onClick={() => toggleSubMenu('purchase')}>
+        <span><i className="fas fa-pen-fancy"></i></span> 구매관리
+      </Link>
+      {openMenu === 'purchase' && (
+        <div className="submenu">
+          <Link to="/purchase-management">거래처</Link>
+          <Link to="/purchase-management">발주 리스트</Link>
+          <Link to="/purchase-management">입고 완료</Link>
+          <Link to="/purchase-management">반품 관리</Link>
+          <Link to="/purchase-management">이슈 관리</Link>
+        </div>
+      )}
+    </div>
+    <div className="menu-item">
+      <Link to="#" onClick={() => toggleSubMenu('material')}>
+        <span><i className="fas fa-project-diagram"></i></span> 자재관리
+      </Link>
+      {openMenu === 'material' && (
+        <div className="submenu">
+          <Link to="/material-management">재고 현황</Link>
+          <Link to="/material-management">사용용 조회</Link>
+          <Link to="/material-management">위치조회/관리</Link>
+          <Link to="/material-management">폐기 관리</Link>
+        </div>
+      )}
+    </div>
+    <div className="menu-item">
+      <Link to="#" onClick={() => toggleSubMenu('quality')}>
+        <span><i className="fas fa-folder-open"></i></span> 품질관리
+      </Link>
+      {openMenu === 'quality' && (
+        <div className="submenu">
+          <Link to="/quality-management">BOM 조회</Link>
+          <Link to="/quality-management">신규등록</Link>
+          <Link to="/quality-management">BOM수정</Link>
+          <Link to="/quality-management">수정이력관리</Link>
+        </div>
+      )}
+    </div>
+    <div className="menu-item">
+      <Link to="#" onClick={() => toggleSubMenu('production')}>
+        <span><i className="fas fa-chart-pie"></i></span> 생산관리
+      </Link>
+      {openMenu === 'production' && (
+        <div className="submenu">
+          <Link to="/production-management">금일 생산(예정)</Link>
+          <Link to="/production-management">전일 생산(완료)</Link>
+          <Link to="/production-management">차주 생산 예정</Link>
+          <Link to="/production-management">계획수량 계산</Link>
+        </div>
+      )}
+    </div>
   </div>
 );
 
 const LoadMenu = () => {
   const [classHolder, setClassHolder] = useState('full-width');
+  const [openMenu, setOpenMenu] = useState('');
 
   const toggleMenu = () => {
     setClassHolder(classHolder === 'full-width' ? 'no-width' : 'full-width');
+  };
+
+  const toggleSubMenu = (menu) => {
+    setOpenMenu(openMenu === menu ? '' : menu);
   };
 
   return (
     <Menu classList={classHolder}>
       <MenuTitle clickEvt={toggleMenu} />
       <SearchBar />
-      <MenuList />
+      <MenuList toggleSubMenu={toggleSubMenu} openMenu={openMenu} />
     </Menu>
   );
 };
