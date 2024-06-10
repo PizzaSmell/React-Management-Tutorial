731f4a889dd2d34ecf1967f7b8eda9c97821a429
JKM <gyungm@gmail.com>
2024-06-10 17:39:36 +0900

    feat.색상불일치 변경 및 관리페이지 연결 시도

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -8,7 +8,7 @@ function Welcome() {
     const navigate = useNavigate();
 
     const handleNavigation = (path) => {
-        navigate(path);
+        navigate(`/layout${path}`);
     };
 
     return (
