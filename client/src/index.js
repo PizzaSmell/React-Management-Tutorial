7b13fdd72c4890dc69f5118caf3a9a0eb1deb8f8
JKM <gyungm@gmail.com>
2024-06-10 19:07:13 +0900

    feat.로그인 실패하여 라우터 수정후 재시도

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // 확인
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement); // 확인
+const root = ReactDOM.createRoot(rootElement);
 
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <App />
     </React.StrictMode>
 );
