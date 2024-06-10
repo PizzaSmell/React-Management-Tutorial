b7ed66be129b694a435fee76b47ba351184ae5da
JKM <gyungm@gmail.com>
2024-06-10 18:42:22 +0900

    feat.reateRoot를 react-dom/client에서 가져오는 걸로 수정

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // 수정된 부분
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
-ReactDOM.createRoot(rootElement).render(
+const root = ReactDOM.createRoot(rootElement); // 수정된 부분
+
+root.render(
     <React.StrictMode>
         <BrowserRouter>
             <App />
