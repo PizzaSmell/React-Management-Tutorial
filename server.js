7dbf191b4b0b178786768154bfab5cb807c3b34e
JKM <gyungm@gmail.com>
2024-06-11 01:04:53 +0900

    feat.이전커밋에서 server.js를 잘못건드려 로그인 실패. 다시 재수정해봄

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const db = mysql.createConnection({
-    host: 'your-db-host',
-    user: 'your-db-user',
-    password: 'your-db-password',
-    database: 'your-db-name'
+    host: 'management-rodem.cx42gsaqw602.ap-southeast-2.rds.amazonaws.com',
+    user: 'admin',
+    password: 'wlsrudals86!',
+    database: 'management_db'
 });
 
 db.connect(err => {
@@ -25,6 +25,7 @@ db.connect(err => {
 // 로그인 엔드포인트
 app.post('/api/login', (req, res) => {
     const { userid, password } = req.body;
+    console.log(`Login attempt with userid: ${userid} and password: ${password}`);
     const query = 'SELECT name, team FROM users WHERE userid = ? AND password = ?';
     
     db.query(query, [userid, password], (err, results) => {
@@ -35,8 +36,10 @@ app.post('/api/login', (req, res) => {
         }
 
         if (results.length > 0) {
+            console.log(`Login successful for userid: ${userid}`);
             res.json({ message: 'Login successful', name: results[0].name, team: results[0].team });
         } else {
+            console.log(`Login failed for userid: ${userid}`);
             res.status(401).send('Invalid credentials');
         }
     });
@@ -63,6 +66,6 @@ app.get('/api/user-info', (req, res) => {
 });
 
 const PORT = 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
