008e1dcfc8d56326d1e070f98323952993a50c89
JKM <gyungm@gmail.com>
2024-06-11 00:46:42 +0900

    feat.웰컴페이지 전체삭제

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,43 +1,8 @@
 import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
 import './Welcome.css';
 
 function Welcome() {
-    const location = useLocation();
-    const { name, team } = location.state || { name: '사용자', team: '팀' };
-    const navigate = useNavigate();
-
-    const handleNavigation = (path) => {
-        navigate(path, { state: { name, team } });
-    };
-
-    return (
-        <div className="welcome-page">
-            <div className="welcome-text">
-                <h1>Welcome, {name}({team})!</h1>
-            </div>
-            <div className="menu-layout">
-                <div className="menu-container">
-                    <div className="menu-item" onClick={() => handleNavigation('/purchase-management')}>
-                        <div className="icon">★</div>
-                        <div className="label">구매관리</div>
-                    </div>
-                    <div className="menu-item" onClick={() => handleNavigation('/material-management')}>
-                        <div className="icon">⬢</div>
-                        <div className="label">자재관리</div>
-                    </div>
-                    <div className="menu-item" onClick={() => handleNavigation('/quality-management')}>
-                        <div className="icon">▲</div>
-                        <div className="label">품질관리</div>
-                    </div>
-                    <div className="menu-item" onClick={() => handleNavigation('/production-management')}>
-                        <div className="icon">◆</div>
-                        <div className="label">생산관리</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+    return null;
 }
 
 export default Welcome;
