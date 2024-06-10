be20060a82ce98c662af4e4ce29613094f33f47e
JKM <gyungm@gmail.com>
2024-06-11 00:53:48 +0900

    feat.웰컴박스 클릭구현

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Welcome.css';
 
 function Welcome() {
+    const navigate = useNavigate();
+
+    const handleNavigation = (path) => {
+        navigate(path);
+    };
+
     return (
         <div className="welcome-container">
-            <div className="box"></div>
-            <div className="box"></div>
-            <div className="box"></div>
-            <div className="box"></div>
+            <div className="box" onClick={() => handleNavigation('/purchase-management')}>구매관리</div>
+            <div className="box" onClick={() => handleNavigation('/material-management')}>자재관리</div>
+            <div className="box" onClick={() => handleNavigation('/quality-management')}>품질관리</div>
+            <div className="box" onClick={() => handleNavigation('/production-management')}>생산관리</div>
         </div>
     );
 }
