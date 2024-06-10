0e7d1f778ff3d87b8d01291cd6ac594ed67002f9
JKM <gyungm@gmail.com>
2024-06-11 00:05:00 +0900

    feat.웰컴페이지 원본텍스트 복구

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,17 +1,39 @@
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import './Welcome.css';
 
-const Welcome = () => {
-  return (
-    <div className="welcome">
-      <div className="box-container">
-        <div className="box">Box 1</div>
-        <div className="box">Box 2</div>
-        <div className="box">Box 3</div>
-        <div className="box">Box 4</div>
-      </div>
-    </div>
-  );
-};
+function Welcome() {
+    const location = useLocation();
+    const { name, team } = location.state || { name: '사용자', team: '팀' };
+    const navigate = useNavigate();
+
+    const handleNavigation = (path) => {
+        navigate(path, { state: { name, team } });
+    };
+
+    return (
+        <div className="welcome-container">
+            <h1>Welcome, {name}({team})!</h1>
+            <div className="menu-container">
+                <div className="menu-item" onClick={() => handleNavigation('/purchase-management')}>
+                    <div className="icon">★</div>
+                    <div className="label">구매관리</div>
+                </div>
+                <div className="menu-item" onClick={() => handleNavigation('/material-management')}>
+                    <div className="icon">⬢</div>
+                    <div className="label">자재관리</div>
+                </div>
+                <div className="menu-item" onClick={() => handleNavigation('/quality-management')}>
+                    <div className="icon">▲</div>
+                    <div className="label">품질관리</div>
+                </div>
+                <div className="menu-item" onClick={() => handleNavigation('/production-management')}>
+                    <div className="icon">◆</div>
+                    <div className="label">생산관리</div>
+                </div>
+            </div>
+        </div>
+    );
+}
 
 export default Welcome;
