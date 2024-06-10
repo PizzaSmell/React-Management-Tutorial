69277ad18579b94c883659d5870b01f3263e386d
JKM <gyungm@gmail.com>
2024-06-10 19:09:48 +0900

    feat. Layout.js 파일을 수정하여 location.state를 정의

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,25 +1,62 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
-import SideMenu from './SideMenu';
+import { useLocation, useNavigate, Outlet } from 'react-router-dom';
 import './Layout.css';
 
-const Layout = ({ user, onLogout }) => {
+function Layout() {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const { name, team } = location.state || {}; // Ensure location.state is defined
+
+    const handleLogout = () => {
+        navigate('/');
+    };
+
     return (
         <div className="layout">
-            <SideMenu />
+            <div className="side-menu">
+                <div className="menu-item">
+                    <span>구매관리</span>
+                    <div className="submenu">
+                        <a href="#">거래처</a>
+                        <a href="#">발주리스트</a>
+                        <a href="#">입고완료</a>
+                        <a href="#">반품관리</a>
+                        <a href="#">이슈관리</a>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <span>자재관리</span>
+                    <div className="submenu">
+                        <a href="#">하위 메뉴 1</a>
+                        <a href="#">하위 메뉴 2</a>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <span>품질관리</span>
+                    <div className="submenu">
+                        <a href="#">하위 메뉴 1</a>
+                        <a href="#">하위 메뉴 2</a>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <span>생산관리</span>
+                    <div className="submenu">
+                        <a href="#">하위 메뉴 1</a>
+                        <a href="#">하위 메뉴 2</a>
+                    </div>
+                </div>
+            </div>
             <div className="content">
                 <header>
                     <div className="user-info">
-                        {user.name}({user.team})님 접속!
-                        <button onClick={onLogout}>로그아웃</button>
+                        <span>{name}({team})님 접속!</span>
+                        <button onClick={handleLogout}>로그아웃</button>
                     </div>
                 </header>
-                <main>
-                    <Outlet />
-                </main>
+                <Outlet />
             </div>
         </div>
     );
-};
+}
 
 export default Layout;
