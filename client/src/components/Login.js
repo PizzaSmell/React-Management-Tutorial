a07ed8644132c5ddb01b015b546c6311f74ebca4
JKM <gyungm@gmail.com>
2024-06-11 01:32:43 +0900

    Revert "feat.로그인 안되어 네비게이션 경로 웰컴으로 설정"
    This reverts commit f50efa64239f0f3af8e8426be2ca682636925a0d.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -17,11 +17,16 @@ const Login = () => {
             },
             body: JSON.stringify({ userid, password }), // 로그인 정보 전송
         })
-            .then((response) => response.json())
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
             .then((data) => {
                 if (data.message === 'Login successful') {
                     console.log('Login successful:', userid);
-                    navigate('/welcome', { state: { userid: userid } }); // 로그인 성공 시 Welcome 페이지로 이동
+                    navigate('/', { state: { userid: userid } }); // 로그인 성공 시 메인 페이지로 이동
                 } else {
                     alert('Invalid credentials'); // 로그인 실패 시 경고
                 }
@@ -35,15 +40,19 @@ const Login = () => {
     return (
         <div className="login-container">
             <form onSubmit={handleSubmit}>
-                <h2>RODEMFOOD</h2>
-                <div>
-                    <label>UserID(이름):</label>
-                    <input type="text" value={userid} onChange={(e) => setUserid(e.target.value)} required />
-                </div>
-                <div>
-                    <label>비밀번호:</label>
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-                </div>
+                <h2>Login</h2>
+                <input
+                    type="text"
+                    placeholder="Userid"
+                    value={userid}
+                    onChange={(e) => setUserid(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
                 <button type="submit">Login</button>
             </form>
         </div>
